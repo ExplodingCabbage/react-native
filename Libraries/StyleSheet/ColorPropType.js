4ddb80ffacf7b26ee98cf60995dd9094fd0b1fe7
Mark Amery <markamery@btinternet.com>
2015-12-19 00:41:32 +0000

    Fix first draft of ColorPropType
    - Add @providesModule annotation for Packager
    - Fix eslint errors

--- Libraries/StyleSheet/ColorPropType.js
+++ Libraries/StyleSheet/ColorPropType.js
@@ -1,21 +1,32 @@
+/**
+ * Copyright (c) 2015-present, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ *
+ * @providesModule ColorPropType
+ * @flow
+ */
 'use strict';
+
 var ReactPropTypes = require('ReactPropTypes');
 var tinycolor = require('tinycolor');
 
 var colorValidator = function (selectedColor, propName) {
-  if( selectedColor === null || selectedColor === undefined ||
-     (typeof selectedColor !== 'string') || selectedColor.trim() === '') {
+  if ((typeof selectedColor !== 'string') || selectedColor.trim() === '') {
     return new Error(
-      `Invalid argument supplied to ${propName}.Expected a string like #123ADF or 'red'.`
-  );
+      `Invalid argument supplied to ${propName}. Expected a string like '#123ADF' or 'red'.`
+    );
   }
 
-  if(tinycolor(selectedColor.trim()).isValid()) {
+  if (tinycolor(selectedColor.trim()).isValid()) {
     return null;
   }
 
   return new Error(
-    `Invalid argument supplied to ${propName}.Expected a string like #123ADF or 'red'.`
+    `Invalid argument supplied to ${propName}. Expected a string like '#123ADF' or 'red'.`
   );
 };
 
